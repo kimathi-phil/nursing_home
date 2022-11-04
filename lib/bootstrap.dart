c433367df72bedee0f2cca7713d001923c07b653
Philip Kimathi <kimathi.phil@gmail.com>
2022-11-04 21:50:14 +0300

    feat: Initialize firebase

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,15 +1,10 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import 'dart:async';
 import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 class AppBlocObserver extends BlocObserver {
   @override
@@ -26,10 +21,13 @@ class AppBlocObserver extends BlocObserver {
 }
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
+  usePathUrlStrategy();
   Bloc.observer = AppBlocObserver();
 
   await runZonedGuarded(
