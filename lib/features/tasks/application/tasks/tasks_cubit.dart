9e929b76b24d243b7870450e2e86b88887121199
Philip Kimathi <kimathi.phil@gmail.com>
2022-11-04 22:29:49 +0300

    feat: Manage tasks state using cubit

---
+++ lib/features/tasks/application/tasks/tasks_cubit.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nursing_home/features/tasks/data/models/task_model.dart';
+import 'package:nursing_home/features/tasks/domain/entities/task.dart';
+import 'package:nursing_home/features/tasks/domain/usecases/usecases.dart';
+import 'package:injectable/injectable.dart';
+
+part 'tasks_state.dart';
+
+@injectable
+class TasksCubit extends Cubit<TasksState> {
+  TasksCubit(
+    this._addNewTaskUseCase,
+    this._getAllTasksUseCase,
+  ) : super(const TasksInitial());
+
+  final AddNewTaskUseCase _addNewTaskUseCase;
+  final GetAllTasksUseCase _getAllTasksUseCase;
+  StreamSubscription? _streamSubscription;
+
+  @override
+  Future<void> close() async {
+    await _streamSubscription?.cancel();
+    await super.close();
+  }
+
+  static TasksCubit get(BuildContext context) => BlocProvider.of(context);
+
+  Future<void> addNewTask(TaskModel task) async {
+    emit(const TasksLoading());
+    await _addNewTaskUseCase(params: task).then((tsk) {
+      emit(TaskSuccess(task));
+    }).catchError((dynamic e) {
+      emit(TasksFailed(e.toString()));
+    });
+  }
+
+  Future<void> getAllTasks(String uid) async {
+    _streamSubscription ??=
+        _getAllTasksUseCase(params: uid).map(TasksSuccess.new).listen(emit);
+  }
+}
