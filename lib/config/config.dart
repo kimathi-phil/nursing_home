450d2f363d0216dc5702b41303b2ffd0b7cbbfd5
Philip Kimathi <kimathi.phil@gmail.com>
2022-11-04 21:44:48 +0300

    chore: Define app routes

---
+++ lib/config/config.dart
@@ -0,0 +1,4 @@
+export 'colors/colors.dart';
+export 'constants/constants.dart';
+export 'routes/routes.dart';
+export 'theme/theme.dart';
---
+++ lib/config/routes/routes.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_playground/features/features.dart';
+import 'package:go_router/go_router.dart';
+
+class AppRouter {
+  const AppRouter._();
+
+  static const signIn = 'login';
+  static const home = 'home';
+  static const todoDetail = 'todoDetail';
+
+  static final GoRouter router = GoRouter(
+    debugLogDiagnostics: kDebugMode,
+    routes: [
+      GoRoute(
+        name: signIn,
+        path: '/signIn',
+        pageBuilder: (context, state) => MaterialPage(
+          key: state.pageKey,
+          child: const SignInScreen(),
+        ),
+      ),
+      GoRoute(
+        name: home,
+        path: '/home',
+        pageBuilder: (context, state) => MaterialPage(
+          key: state.pageKey,
+          child: TasksScreen(uid: state.queryParams['uid']!),
+        ),
+      ),
+    ],
+    initialLocation: '/signIn',
+    errorPageBuilder: (context, state) => MaterialPage(
+      key: state.pageKey,
+      child: ErrorScreen(error: state.error),
+    ),
+  );
+}
+
+class ErrorScreen extends StatelessWidget {
+  const ErrorScreen({super.key, this.error});
+  final Exception? error;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(leading: const CloseButton()),
+      body: const Center(
+        child: Text('Something went wrong!'),
+      ),
+    );
+  }
+}
